1acf61a17ff5b7bf41ec5796a547ebda17216027
hezron006 <202365006@student.unipa.ac.id>
2024-12-13 00:09:55 +0900

    pp

--- BACKEND/Server.js
+++ BACKEND/Server.js
@@ -5,8 +5,22 @@ const bodyParser = require('body-parser')
 const PostRouter = require("./router/PostRouter")
 require('dotenv').config()
 
+app.use(session({
+    secret: 'your-secret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      sameSite: 'None', // Agar bisa bekerja lintas situs
+      secure: true,     // Pastikan Anda menggunakan HTTPS
+    },
+  }));
+  
 
-app.use(cors())
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  }));
+  
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use('/post', PostRouter)

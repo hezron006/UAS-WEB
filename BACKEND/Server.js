d4ce1ac46481f1ed4306dc6036973567e6388d14
hezron006 <202365006@student.unipa.ac.id>
2024-12-13 14:07:22 +0900

    p

--- BACKEND/Server.js
+++ BACKEND/Server.js
@@ -5,22 +5,17 @@ const bodyParser = require('body-parser')
 const PostRouter = require("./router/PostRouter")
 require('dotenv').config()
 
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
 
-
-
-
+  
 
 app.use(cors({
-  origin: 'https://jadwal-lovat.vercel.app', // Ganti dengan domain frontend Anda
-  credentials: true, // Mengizinkan pengiriman cookie dan header Authorization
-}));
-
-// app.use((req, res, next) => {
-//   res.setHeader('Content-Security-Policy', 
-//     "default-src 'none'; script-src 'self' https://vercel.live; img-src 'self' https://api-jadwal-pkl66fmn0-hezron006s-projects.vercel.app; connect-src 'self'; style-src 'self' 'unsafe-inline'; font-src 'self';"
-//   );
-//   next();
-// });
+    origin: 'https://jadwal-lovat.vercel.app',  
+    credentials: true,  
+  }));
+  
+ 
   
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended: true}))
@@ -30,8 +25,8 @@ app.get('/', (req, res) => {
     res.send("Server berjalan dengan baik!");
   });
 
-// const port = process.env.PORT
-module.exports = app 
+const port = process.env.PORT
+module.exports = app || port
 
 
 // app.listen(port, ()=> {

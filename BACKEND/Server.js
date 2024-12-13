7f093269ff316c77bab48e6a1360be2f6574570c
hezron006 <202365006@student.unipa.ac.id>
2024-12-13 13:09:16 +0900

    use

--- BACKEND/Server.js
+++ BACKEND/Server.js
@@ -17,7 +17,10 @@ app.use(cors({
 }));
 
 app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://jadwal-lovat.vercel.app'); // Sesuaikan dengan domain frontend
   res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
   

977a078f75e35ac20cb2160e4d9cb6cc0923e93a
hezron006 <202365006@student.unipa.ac.id>
2024-12-13 13:55:03 +0900

    o

--- BACKEND/Server.js
+++ BACKEND/Server.js
@@ -15,7 +15,12 @@ app.use(cors({
   credentials: true, // Mengizinkan pengiriman cookie dan header Authorization
 }));
 
-
+app.use((req, res, next) => {
+  res.setHeader('Content-Security-Policy', 
+    "default-src 'none'; script-src 'self' https://vercel.live; img-src 'self' https://api-jadwal-pkl66fmn0-hezron006s-projects.vercel.app; connect-src 'self'; style-src 'self' 'unsafe-inline'; font-src 'self';"
+  );
+  next();
+});
   
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended: true}))

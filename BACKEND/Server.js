061b5ec69ea39229ac2fe39fc1f7566403ac9783
hezron006 <202365006@student.unipa.ac.id>
2024-12-13 13:18:35 +0900

    p

--- BACKEND/Server.js
+++ BACKEND/Server.js
@@ -4,6 +4,20 @@ const cors = require ('cors')
 const bodyParser = require('body-parser')
 const PostRouter = require("./router/PostRouter")
 require('dotenv').config()
+const helmet = require('helmet');
+app.use(helmet());
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+      defaultSrc: ["'self'"], // Membolehkan hanya sumber dari domain yang sama
+      scriptSrc: ["'self'", "https://vercel.live"], // Membolehkan skrip dari domain sendiri dan vercel.live
+      imgSrc: ["'self'", "https://api-jadwal.vercel.app"], // Membolehkan gambar dari domain sendiri dan api-jadwal.vercel.app
+      connectSrc: ["'self'", "https://api-jadwal.vercel.app"], // Membolehkan koneksi API ke api-jadwal.vercel.app
+      styleSrc: ["'self'", "'unsafe-inline'"], // Membolehkan CSS dari domain sendiri dan inline styles
+      fontSrc: ["'self'"], // Membolehkan font dari domain sendiri
+      objectSrc: ["'none'"], // Menghindari penggunaan objek (misalnya Flash)
+  },
+}));
+
 
 const cookieParser = require('cookie-parser');
 app.use(cookieParser());
@@ -12,8 +26,8 @@ app.use(cookieParser());
 
 
 app.use(cors({
-  origin: ['https://jadwal-lovat.vercel.app', 'http://localhost:3000'], // Ganti dengan domain frontend Anda
-  credentials: true, // Mengizinkan pengiriman cookie dan header Authorization
+  origin: 'https://jadwal-lovat.vercel.app', // Sesuaikan dengan domain frontend
+  credentials: true, // Izinkan pengiriman cookie dan header Authorization
 }));
 
 app.use((req, res, next) => {
